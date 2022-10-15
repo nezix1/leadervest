c5ecd49b83f02eafc2507bac88231d9aec0e18c5
Ilya <76724295+nezix1@users.noreply.github.com>
2022-10-15 21:16:17 +0300

    1

---
+++ js/app.js
@@ -0,0 +1,16 @@
+(() => {
+    "use strict";
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    $("#loadmore").click((function() {
+        $(".hidden").removeClass("hidden");
+        $(this).hide();
+    }));
+})();
